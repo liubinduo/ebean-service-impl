10ff30cb0e28864fdda4aa712c7d7905af47fd16
liubinduo <liubinduo@v1ok.com>
2021-07-01 16:37:36 +0800

    upload 1290.0.1.13

--- src/main/java/com/v1ok/AutoConfig.java
+++ src/main/java/com/v1ok/AutoConfig.java
@@ -1,5 +1,11 @@
 package com.v1ok;
 
+import com.v1ok.auth.IContext;
+import com.v1ok.auth.IUserContext;
+import com.v1ok.commons.ContextHolder;
+import io.ebean.config.CurrentTenantProvider;
+import io.ebean.config.CurrentUserProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +13,32 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.v1ok.db")
 public class AutoConfig {
 
+  @Bean
+  public CurrentUserProvider userProvider() {
+    return () -> {
+      IContext context = ContextHolder.getHolder().get();
+
+      if (context == null) {
+        return null;
+      }
+      IUserContext userContext = context.currentUser();
+
+      return userContext.getPositions();
+    };
+  }
+
+  @Bean
+  public CurrentTenantProvider tenantProvider() {
+    return () -> {
+      IContext context = ContextHolder.getHolder().get();
+      if (context == null) {
+        return null;
+      }
+      IUserContext userContext = context.currentUser();
+      if (userContext != null) {
+        return userContext.getTenantId();
+      }
+      return "DEFAULT";
+    };
+  }
 }
